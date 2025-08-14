87586425403dda4a75acb457d807bc25b799eb56
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2025-08-14 16:34:19 +0200

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from src.app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello CI/CD' in response.data
